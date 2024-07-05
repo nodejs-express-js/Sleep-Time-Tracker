39b0d4ee3c03515fd298c3a39e22fc920a7be5a0
VISH01AL <g.vish1al@gmail.com>
2024-07-05 17:57:17 +0530

    renamed old frontend to frontend_1 to create everything from scratch to write cleaner code

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# Frontend1
+# Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.4.
 
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { authguardGuard } from './authguard.guard';
-export const routes: Routes = [
-    {path: '', component:HomeComponent,canActivate:[authguardGuard]},
-    {path: 'home', component: HomeComponent,canActivate:[authguardGuard]},
-    {path: 'login', component: LoginComponent},
-    {path:'signup', component: SignupComponent},
-    {path: '**', redirectTo: 'home'}
-];
+
+export const routes: Routes = [];
